9580ed4101dd1c203e957f669000e47a3cffaac1
Roshil Kapatel <roshil.170410116026@gmail.com>
2020-12-02 12:03:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcgNxc5OqELJd-boRrxgqzVdMM4l7iiQY",
-  authDomain: "cl-e2f18.firebaseapp.com",
-  databaseURL: "https://cl-e2f18.firebaseio.com",
-  projectId: "cl-e2f18",
-  storageBucket: "cl-e2f18.appspot.com",
-  messagingSenderId: "617087796074",
-  appId: "1:617087796074:web:9fcb0bcccdcd61aa875b13",
-  measurementId: "G-TBBKSP3CJ2",
+  apiKey: "KEY???",
+  authDomain: "????",
+  databaseURL: "????",
+  projectId: "????",
+  storageBucket: "?????",
+  messagingSenderId: "??????",
+  appId: "??????",
+  measurementId: "????",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
